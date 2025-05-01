b18e82f852b7b572dba992d2c68f1508b1b90182
Matthew Bloch <mjbloch22@gmail.com>
2025-05-01 11:06:27 -0700

    Simplify error handling

--- tap_scout/streams/scout_cases.py
+++ tap_scout/streams/scout_cases.py
@@ -16,69 +16,62 @@ def stream(scout_url, api_key, appointment_ids):
     :return: A list of dictionaries containing the data retrieved from the API.
     """
     for appointment_id in appointment_ids:
+        case = scout_api(scout_url, api_key, appointment_id)
+        if not case:  # Check if the response is empty or None
+            logging.error(f"Error: No data or invalid response for appointment_id: {appointment_id}")
+            continue
+
         try:
-            case = scout_api(scout_url, api_key, appointment_id)
-            if not case:  # Check if the response is empty or None
-                logging.error(f"Error: No data or invalid response for appointment_id: {appointment_id}")
-                continue
+            case = json.loads(case)  # Attempt to parse the JSON
+        except json.JSONDecodeError as e:
+            logging.error(f"Unable to parse JSON for appointment_id: {appointment_id}. Error: {e}")
+            continue
 
-            try:
-                case = json.loads(case)  # Attempt to parse the JSON
-            except json.JSONDecodeError as e:
-                logging.error(f"Unable to parse JSON for appointment_id: {appointment_id}. Error: {e}")
-                continue
+        patient_data = case.get("patient")
 
-            patient_data = case.get("patient")
+        singer.write_record(
+            "scout_cases",
+            {
+                "appointment_id": appointment_id,
+                "procedures": case.get("procedures"),
+                "staff": case.get("staff"),
+                "additional_medication": patient_data.get(
+                    "additionalMedications"
+                ),
+                "anesthetic_gases": patient_data.get("anestheticGases"),
+                "birthdate": patient_data.get("birthdate"),
+                "breed": patient_data.get("breed"),
+                "dental_chartings": patient_data.get("dentalChartings"),
+                "fluid_cri_with_dilutions": patient_data.get(
+                    "fluidCriWithDilutions"
+                ),
+                "fluid_cri_without_dilutions": patient_data.get(
+                    "fluidCriWithoutDilutions"
+                ),
+                "monitoring_vitals": patient_data.get("monitoringVitals"),
+                "name": patient_data.get("name"),
+                "overall_timer": patient_data.get("overallTimer"),
+                "overall_timer_notes": patient_data.get(
+                    "overallTimerNotes"
+                ),
+                "patient_procedures": patient_data.get("patientProcedures"),
+                "pre_anesthetic_medications": patient_data.get(
+                    "preAnestheticMedications"
+                ),
+                "procedure_date": patient_data.get("procedure_date"),
+                "regional_blocks": patient_data.get("regionalBlocks"),
+                "resuscitate": patient_data.get("resuscitate"),
+                "sex": patient_data.get('sex'),
+                "species": patient_data.get("species"),
+                "spayed_or_neutered": patient_data.get(
+                    "spayed_or_neutered"
+                ),
+                "weight": patient_data.get("weight")
+            }
+        )
 
-            singer.write_record(
-                "scout_cases",
-                {
-                    "appointment_id": appointment_id,
-                    "procedures": case.get("procedures"),
-                    "staff": case.get("staff"),
-                    "additional_medication": patient_data.get(
-                        "additionalMedications"
-                    ),
-                    "anesthetic_gases": patient_data.get("anestheticGases"),
-                    "birthdate": patient_data.get("birthdate"),
-                    "breed": patient_data.get("breed"),
-                    "dental_chartings": patient_data.get("dentalChartings"),
-                    "fluid_cri_with_dilutions": patient_data.get(
-                        "fluidCriWithDilutions"
-                    ),
-                    "fluid_cri_without_dilutions": patient_data.get(
-                        "fluidCriWithoutDilutions"
-                    ),
-                    "monitoring_vitals": patient_data.get("monitoringVitals"),
-                    "name": patient_data.get("name"),
-                    "overall_timer": patient_data.get("overallTimer"),
-                    "overall_timer_notes": patient_data.get(
-                        "overallTimerNotes"
-                    ),
-                    "patient_procedures": patient_data.get("patientProcedures"),
-                    "pre_anesthetic_medications": patient_data.get(
-                        "preAnestheticMedications"
-                    ),
-                    "procedure_date": patient_data.get("procedure_date"),
-                    "regional_blocks": patient_data.get("regionalBlocks"),
-                    "resuscitate": patient_data.get("resuscitate"),
-                    "sex": patient_data.get('sex'),
-                    "species": patient_data.get("species"),
-                    "spayed_or_neutered": patient_data.get(
-                        "spayed_or_neutered"
-                    ),
-                    "weight": patient_data.get("weight")
-                }
-            )
-
-            # Rate limiting to prevent error of duplicate data
-            # being written for different appointment_ids
-            time.sleep(1)
-
-        except Exception as e:
-            logging.error(
-                f"Unexpected error for appointment_id: {appointment_id}. Error: {e}",
-                exc_info=True
-            )
+        # Rate limiting to prevent error of duplicate data
+        # being written for different appointment_ids
+        time.sleep(1)
 
     logging.info("Completed scout_cases.py")
